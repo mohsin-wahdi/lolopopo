e6b8b9313ecb4f241f33d56edb939a966f7b62d3
mohsin-wahdi <amman4781@gmail.com>
2024-12-02 10:33:06 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
+const session = require("express-session");
 
 /* GET home page. */
-router.get("/", function (req, res) {
-  res.render("index");
-});
+// router.get("/", function (req, res) {
+//   res.render("index");
+// });
 
 //CRUD Opertion is start
 
@@ -32,11 +33,35 @@ router.get("/", function (req, res) {
 // });
 
 // delete the data in db
-router.get("/delete", async function (req, res) {
-  let deleteuser = await userModel.findOneAndDelete({
-    username: "Aman",
+// router.get("/delete", async function (req, res) {
+//   let deleteuser = await userModel.findOneAndDelete({
+//     username: "Aman",
+//   });
+//   res.send(deleteuser);
+// });
+
+/*Create the session*/
+//Declaration the session
+router.get("/", function (req, res) {
+  req.session.Koibhiname = true;
+  res.render("index");
+});
+
+//Read the session
+router.get("/cheakban", function (req, res) {
+  if (req.session.Koibhiname === true) {
+    res.send("You are banned");
+  } else {
+    res.send("not banned");
+  }
+});
+
+//Delete the session
+router.get("/removeban", function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) throw err;
+    res.send("Ban removed");
   });
-  res.send(deleteuser);
 });
 
 module.exports = router;
